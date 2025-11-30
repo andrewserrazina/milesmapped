cc87a7addb3c1ab22a1bdb3d417e89d854087122
andrewserrazina <aserrazina101@gmail.com>
2025-11-30 17:00:54 -0500

    Fix build error for prerendered error pages

--- milesmapped-frontend/app/layout.tsx
+++ milesmapped-frontend/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "../components/NavBar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MilesMapped | Travel with Points and Miles",
@@ -34,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <body>
         <NavBar />
         <main className="px-6 py-10">
